fcf555365253394e635feb9467d5f980fda6da2c
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-03-19 14:02:53 +0300

    add user entity

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  before_save { email.downcase! }
+  validates :name, presence: true, length: { in: 4..12 }
+  EMAIL_REGEXP = /^.+@.+$/
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
+end
